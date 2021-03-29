8493e62f630dde9eb39b1b1dc5b516351ef8a5be
Gabe Ulrich Laptop <gabriel.ulrich@aol.com>
2021-03-29 11:22:11 -0400

    Added video pop up

--- data_package/extra.js
+++ data_package/extra.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
     current_year_insert();
+	$(".pop_up_video").removeClass('d-none');  
+	$(".pop_up_video").hide();
+	setTimeout(
+	  function() 
+	  {
+		
+		$(".pop_up_video").slideDown("slow");
+	  }, 5000);
    });
+
+
+function close_pop_up(){
+	$(".pop_up_video").slideUp("slow");
+}
 
 function current_year_insert() {
     var d = new Date();
